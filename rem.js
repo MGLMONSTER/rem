d1d0a2c06b83f79281637912fd8ecb4a7a5e769f
MGLMONSTER <mglmonster151@gmail.com>
2019-01-10 11:05:27 +0800

    add rem.js file

---
+++ rem.js
@@ -0,0 +1,17 @@
+//根据屏幕的宽度来调节根元素（html）的 font-size 值
+(function (doc, win) {
+    var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+                var clientWidth = docEl.clientWidth;
+                if (!clientWidth) return;
+                if(clientWidth>=640){
+                    docEl.style.fontSize = '100px';
+                }else{
+                    docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+                }
+            };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
